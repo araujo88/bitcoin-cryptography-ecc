9470fc35767d02f15bdfa517468c45337b2e7dd8
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2022-12-01 10:01:28 -0300

    Update ex4_9.py

--- ex4_9.py
+++ ex4_9.py
@@ -6,4 +6,4 @@ secret = little_endian_to_int(hash256(passphrase))
 priv = PrivateKey(secret)
 print(priv.point.address(testnet = True))
 
-'''Address: mkHc2s6q11EqgwLGdg9Adh6qM7C6yUfXqv'''
+'''Address: mt4o1EhS4SCgZ6gZHXETpnakykTfaa1Acc'''
